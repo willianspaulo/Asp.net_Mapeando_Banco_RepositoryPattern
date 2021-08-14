d7959e445deffa9506b1427aababc5e5e690d41d
Willians Paulo da Silva <willianspaulo@live.com>
2021-08-14 20:11:59 -0300

    Cria projeto Domain e Repository

---
+++ VxTel.Repository/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VxTel.Repository
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
