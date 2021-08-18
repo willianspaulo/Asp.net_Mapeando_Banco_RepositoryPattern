a3b089d0f368ffbf692a5d2c44add472a091a98c
Willians Paulo da Silva <willianspaulo@live.com>
2021-08-18 19:45:59 -0300

    Mapeia banco de dados no projeto Model (Cria Context)

--- VxTel.Api/Startup.cs
+++ VxTel.Api/Startup.cs
@@ -27,7 +27,7 @@ namespace VxTel.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<ITeste, TesteRepository>();
+            services.AddScoped<ITeste, TesteRepository>();  
 
             services.AddControllers();
         }
---
+++ VxTel.Domain/Models/VxTelContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace VxTel.Domain.Models
+{
+    public partial class VxTelContext : DbContext
+    {
+        public VxTelContext()
+        {
+        }
+
+        public VxTelContext(DbContextOptions<VxTelContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer("Server=.;Database=VxTel;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Latin1_General_CI_AS");
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
